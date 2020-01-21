680ef8fb663bfa91a30d149cd066c4e13b332059
tcdmv <bamchaar@gmail.com>
2020-01-21 11:43:19 -0500

    added com.mortgsgecalculator project

---
+++ com.mortgagecalculator/src/main/java/browserSetup.java
@@ -0,0 +1,139 @@
+
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Properties;
+import java.sql.Connection;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+
+
+
+
+
+public class browserSetup {
+	static WebDriver driver,Br;
+	static String url;
+	static String browser;
+	
+	private static String urlc;
+	private static String user;
+	private static String password;
+	static Connection con;
+	static Statement st ;
+	static ResultSet result ;
+	
+	public browserSetup(WebDriver driver, String browser,String url, Connection con) throws Exception{
+		
+		this.driver = driver;
+		this.browser = browser;
+		this.url = url;
+		this.con= con;
+		//dbconnect();
+		setup();
+		
+	}
+		
+		public void setup() throws IOException, InterruptedException {
+		
+		Properties ppt = new Properties();
+		File filepath = new File(System.getProperty("user.dir")+"\\target\\Resources\\config.properties");
+		//com.mortgagecalculator/target/Resources/config.properties
+		FileInputStream file = new FileInputStream(filepath);
+		ppt.load(file);
+		browser = ppt.getProperty("browser");
+		url =ppt.getProperty("url");
+		
+		 if (browser.equalsIgnoreCase("chrome")) {
+		String key , browserPath;
+		key = ppt.getProperty("chromekey");
+		browserPath = ppt.getProperty("browserPathchrome");
+		System.setProperty(key, browserPath);
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	
+		 }
+		 if (browser.equalsIgnoreCase("Firefox")) {
+				String key , browserPath;
+				key = ppt.getProperty("ffxkey");
+				browserPath = ppt.getProperty("browserPathffx");
+				System.setProperty(key, browserPath);
+				driver = new FirefoxDriver();
+				driver.manage().window().maximize();
+				
+				 }
+		 if (browser.equalsIgnoreCase("Edge")) {
+				String key , browserPath;
+				key = ppt.getProperty("msekey");
+				browserPath = ppt.getProperty("browserPathmse");
+				System.setProperty(key, browserPath);
+				Thread.sleep(3000);
+				driver = new EdgeDriver();
+				
+				driver.manage().window().maximize();
+				
+				 }
+		 if (browser.equalsIgnoreCase("ie")) {
+				String key , browserPath;
+				key = ppt.getProperty("iekey");
+				browserPath = ppt.getProperty("browserie");
+				System.setProperty(key, browserPath);
+				driver = new InternetExplorerDriver();
+				driver.manage().window().maximize();
+				
+				 }
+	}
+		
+		public void dbconnect() throws Exception {
+			 urlc = "jdbc:mysql://localhost:3307/sakila";
+			 user = "Abdel"; 
+			password = "$123Abcd";
+			
+			Class.forName("com.mysql.jdbc.Driver");
+
+			con =DriverManager.getConnection(urlc,user,password);
+			
+			Statement st = con.createStatement();
+			ResultSet result = st.executeQuery("select * from actor");
+			
+			java.sql.ResultSetMetaData rsmd = result.getMetaData();
+			   System.out.println("querying SELECT * FROM XXX");
+			
+			int columnsNumber = rsmd.getColumnCount();
+			   while (result.next()) {
+			       for (int i = 1; i <= columnsNumber; i++) {
+			           if (i > 1) System.out.print(",  ");
+			           String columnValue = result.getString(i);
+			           System.out.print(columnValue + " " + rsmd.getColumnName(i));
+			       }
+			       System.out.println("");
+			   }
+
+			 try {
+		            if (result != null) {
+		                result.close();
+		            }
+
+		            if (st != null) {
+		                st.close();
+		            }
+
+		            if (con != null) {
+		                con.close();
+		            }
+		        } catch (Exception e) {
+
+		        }
+			
+			
+		}
+}
---
+++ com.mortgagecalculator/src/main/java/testbrowser.java
@@ -0,0 +1,69 @@
+
+
+import java.io.File;
+import java.sql.Connection;
+import java.util.Iterator;
+import java.util.Set;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class testbrowser extends browserSetup {
+
+	public testbrowser(WebDriver driver, String browser, String url,Connection con) throws Exception {
+		super(driver, browser,url,con);
+		
+	}
+
+	public static void main(String[] args) throws Exception {
+		
+		
+		
+		testbrowser session = new testbrowser(driver,browser,url,con);
+		driver.get(url);
+	    
+		driver.findElement(By.name("defaultbutton")).click();
+		
+		Set windowset = driver.getWindowHandles();
+		System.out.println(windowset);
+		System.out.println(driver.getTitle());
+		
+		Iterator it = windowset.iterator();
+		
+		driver.switchTo().window((String) it.next());
+		driver.switchTo().window((String) it.next());
+		
+		System.out.println(driver.getTitle());
+		
+		driver.findElement(By.className("fmcty-radio-option")).click();
+		driver.findElement(By.id("MortgageCalculatorDOTORG_property_zip")).sendKeys("20147");
+		
+		Thread.sleep(2000);
+		System.out.println(driver.getTitle());
+		
+		TakesScreenshot ts = (TakesScreenshot)driver;
+		File source = ts.getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(source, new File("./Screenshots/Screen.png"));
+		System.out.println("the Screenshot is taken");
+		Set windowset1 = driver.getWindowHandles();
+		Iterator it1 = windowset1.iterator();
+		driver.switchTo().window((String) it1.next());
+		
+		
+		System.out.println(driver.getTitle());
+		Thread.sleep(3000);
+		
+		
+		driver.findElement(By.id("homeval")).sendKeys("500000");
+		
+	
+		
+
+		
+
+	}
+
+}
